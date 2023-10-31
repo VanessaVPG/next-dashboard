743b938afd7de815c95eb9bee2f9bc6baaad9c67
vanessavpg <vanessa.pg026@gmail.com>
2023-10-31 19:41:23 -0300

    fix: middleware

--- auth.config.ts
+++ auth.config.ts
@@ -12,7 +12,6 @@ export const authConfig = {
       if (isOnDashboard) {
         if (isLoggedIn) return true;
         return false; 
-      } else if (isLoggedIn) {
         return Response.redirect(new URL('/dashboard', nextUrl));
       }
       return true;
